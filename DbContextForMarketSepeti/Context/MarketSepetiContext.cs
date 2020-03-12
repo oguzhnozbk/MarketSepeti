1b87acd943707cd897d5698f08a6359c0e88ad8f
Oguzhan <oguzhnozbk@gmail.com>
2020-03-12 16:20:23 +0300

    DbContext layerd created for Market Sepeti Entities

---
+++ DbContextForMarketSepeti/Context/MarketSepetiContext.cs
@@ -0,0 +1,20 @@
+﻿using EntitiesForMarketSepeti.Entities;
+using System.Data.Entity;
+
+namespace DbContextForMarketSepeti.Context
+{
+    public class MarketSepetiContext : DbContext
+    {
+        public MarketSepetiContext() : base("MarketSepetiDb")
+        {
+
+        }
+        public virtual DbSet<Market> Markets { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<ProductMarket> ProductMarkets { get; set; }
+        public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public virtual DbSet<ShoppingCartProductMarket> ShoppingCartProductMarkets { get; set; }
+    }
+}
